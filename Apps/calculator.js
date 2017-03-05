00f5b16473596704bba3bd0e08bdf8b2dc61bd58
Phat Chiem <thachphat0@gmail.com>
2017-03-05 12:34:17 +0700

    Initial Calculator

---
+++ Apps/calculator.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput
+} from 'react-native';
+
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+
+export default class Cal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      segmentSelectedIndex : 0,
+      billAmount : 0,
+      result: 0,
+      tipAmount: 0
+    }
+  }
+
+  handleSegmentChange(index) {
+    this.setState({
+      segmentSelectedIndex : index
+    })
+
+    this.handleBillAmountChange(this.state.billAmount, index)
+  }
+
+  handleBillAmountChange(bill, index) {
+    this.setState({
+      billAmount : bill
+    })
+
+    if (!index && index != 0) {
+      index = this.state.segmentSelectedIndex;
+    }
+
+    bill = parseFloat(bill);
+    var percent = this.segmentValues()[index];
+    percent = parseFloat(percent) / 100;
+
+    var result = bill + bill * percent;
+    this.setState({
+      result: result,
+      tipAmount: bill * percent
+    })
+  }
+
+  segmentValues() {
+    return ['10%', '15%', '50%'];
+  }
+
+  render() {
+    return(
+      <View>
+        <View>
+          <Text>Tip Calculator</Text>
+        </View>
+
+        <View>
+          <Text>Bill Amount</Text>
+          <TextInput
+            onChangeText={(billAmount) => this.handleBillAmountChange(billAmount)}
+            keyboardType='numeric'
+            maxLength={10}
+          />
+        </View>
+
+        <View>
+          <Text>Tip Amount : 0</Text>
+        </View>
+
+        <View>
+          <SegmentedControlTab
+            values={this.segmentValues()}
+            onTabPress= {index => this.handleSegmentChange(index)}
+          />
+        </View>
+
+        <View>
+          <Text>Bill input : {this.state.billAmount}</Text>
+          <Text>Tip amount : {this.state.tipAmount}</Text>
+          <Text>Segment control : {this.segmentValues()[this.state.segmentSelectedIndex]}</Text>
+        </View>
+
+        <View>
+          <Text>Result : {this.state.result}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+module.exports = Cal
